d4f9d4a90c9a9e0ad9935ce2acee17352dc8462e
a2jensen <a2jensen@ucsd.edu>
2025-04-17 00:37:34 -0700

    Added Vercel Analytics

--- app/experience/section.tsx
+++ app/experience/section.tsx
@@ -13,28 +13,6 @@ const Experience = () => {
                     <li className="border-l-2 border-white pl-4 py-2">
                         <h3 className="text-xl font-semibold">WebDev Lead @ KP</h3>
                     </li>
-                    <li className="border-l-2 border-white pl-4 py-2">
-                        <h3 className="text-xl font-semibold">Radio DJ @ KSDT</h3>
-                        <ul className="flex flex-wrap">
-                            {[ 
-                                "4yfOwEFEH9Qmn0bbtffXGu",
-                                "3XnBhHs1pE0czLTrmD0tDC",
-                                "2absBchLXC2OHsIFHtSIht",
-                                "36jmtwv9117ifHj4NMGB9P"
-                            ].map((id) => (
-                                <li key={id} className="w-full md:w-1/2 lg:w-2/4 p-2">
-                                <iframe
-                                    className=" mt-5"
-                                    src={`https://open.spotify.com/embed/playlist/${id}?utm_source=generator`}
-                                    width="100%"
-                                    height="352"
-                                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                                    loading="lazy"
-                                />
-                                </li>
-                            ))}
-                            </ul>
-                    </li>
                 </ul>
             </div>
         </div>
