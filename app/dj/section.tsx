a6218583516a8b0348e829ad4eaef99f2f024e22
a2jensen <a2jensen@ucsd.edu>
2025-04-17 00:47:58 -0700

    Edited radio section

--- app/dj/section.tsx
+++ app/dj/section.tsx
@@ -1,9 +1,8 @@
 const DJ = () => {
     return (
-            <div id="dj" className="container mx-auto px-8">
-                    <li className="border-l-2 border-white pl-4 py-2">
-                        <h3 className="text-xl font-semibold">Radio DJ @ KSDT // Tuesdays 6pm</h3>
-                        <ul className="flex flex-wrap">
+            <div id="dj" className="container bg-black text-white mx-auto px-8">
+                <h3 className="text-xl font-semibold my-8">Radio DJ @ KSDT // Tuesdays 6pm</h3>
+                        <ul className="flex flex-wrap border-l-2 border-white pl-4 py-2">
                             {[ 
                                 "4yfOwEFEH9Qmn0bbtffXGu",
                                 "3XnBhHs1pE0czLTrmD0tDC",
@@ -22,7 +21,6 @@ const DJ = () => {
                                 </li>
                             ))}
                             </ul>
-                    </li>
             </div>
     )
 }
