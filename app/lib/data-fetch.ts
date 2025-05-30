dfcee00407962b1827fcb71a255667575939cd3d
Angelo Jensen <a2jensen@ucsd.edu>
2025-05-30 02:13:35 -0700

    added data cache

--- app/lib/data-fetch.ts
+++ app/lib/data-fetch.ts
@@ -2,14 +2,14 @@
 
 export async function dataFetch(){
     try {
-        const link = process.env.SPREADSHEETS_URL
+        const link = process.env.SPREADSHEETS_URL;
         if (!link){
              throw new Error("ENVIRONMENT variables not valid")
         }
-         const response = await fetch(link, { cache: 'no-store' });
+         const response = await fetch(link, { next: {revalidate: 3600} });
         
         if (!response){
-            throw new Error("Response is invalid")
+            throw new Error("Response is invalid");
         }
 
         const data = await response.json();
@@ -17,7 +17,7 @@ export async function dataFetch(){
         return data;
 
     } catch (error : unknown) {
-        console.error("Failed to fetch", error)
+        console.error("Failed to fetch", error);
         throw error;
     }
 }
