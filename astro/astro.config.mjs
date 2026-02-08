c4ef4eb187ae6d5c65b39f41c5e614ccd8180391
angelo <angelojensen@gmail.com>
2026-02-07 19:48:29 -0800

    Initial version

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    experimental: {
+        fonts: [{
+            provider: fontProviders.google(),
+            name: "Roboto",
+            cssVariable: "--font-roboto"
+        },
+        {
+            provider: fontProviders.google(),
+            name: "JetBrains Mono",
+            cssVariable:"--font-jetbrains-mono"
+        }
+    ]
+    }
+});
