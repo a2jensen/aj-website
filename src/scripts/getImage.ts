ca9d8f312821a954574e86f1f214e698cd17cc16
angelo <angelojensen@gmail.com>
2026-03-24 14:47:59 -0700

    removed public objects and logging

--- src/scripts/getImage.ts
+++ src/scripts/getImage.ts
@@ -3,10 +3,10 @@ const images = import.meta.glob<{ default: ImageMetadata }>('../images/*.*', { e
 export function getImage(path: string) {
 	const filename = path.split('/').pop()!;
 	const match = Object.entries(images).find(([key]) => key.endsWith(filename));
-	if (match) {
-		console.log(`[getImage] Found: ${filename}`);
-	} else {
-		console.warn(`[getImage] Missing: ${filename} (path: ${path})`);
-	}
+	// if (match) {
+	// 	console.log(`[getImage] Found: ${filename}`);
+	// } else {
+	// 	console.warn(`[getImage] Missing: ${filename} (path: ${path})`);
+	// }
 	return match ? match[1].default : undefined;
 }
